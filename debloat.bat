c985f1967aee7198d572c5e4dc77f45a010cd730
isocolon <70964355+isocolon@users.noreply.github.com>
2020-11-11 11:10:31 +0800

    Update debloat.bat

--- debloat.bat
+++ debloat.bat
@@ -14,3 +14,10 @@ if %errorLevel% == 0 (
 :: Remove 'Cast to Device...' from context menu
 ::
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" /V {7AD84985-87B4-4a16-BE58-8B72A5B390F7} /t REG_SZ /d "Play to Menu" /f
+
+
+::
+:: Remove 3D Objects
+::
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}" /f
+reg delete "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}" /f
