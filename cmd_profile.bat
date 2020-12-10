da12038aa686516ad610c8601a671d397253aca5
isocolon <70964355+isocolon@users.noreply.github.com>
2020-12-10 08:05:27 +0800

    Update cmd_profile.bat

--- cmd_profile.bat
+++ cmd_profile.bat
@@ -5,3 +5,5 @@ setlocal EnableDelayedExpansion
 set ALIASFILE="%USERPROFILE%\config\cmd_profile.bat"
 set SUBLIME="%ProgramFiles%\Sublime Text 3\sublime_text.exe"
 set SYS32="\Windows\System32"
+
+doskey dt=cd /D "%USERPROFILE%\Desktop"
